c2ac28cbebf9caf2078a455aac7daa90f6545411
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-08-21 11:15:15 -0300

    .

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -17,27 +17,36 @@ function verificar() {
             genero = 'Homem'
             if (idade >=0 && idade < 10) {
                 // criança
+                img.setAttribute('src', 'crianca-h.png')
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'jovem-h.png')
             } else if (idade < 60) {
                 //adulto
+                img.setAttribute('src', 'adulto-h.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso-h.png')
             }
 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 // criança
+                img.setAttribute('src', 'crianca-m.png')
             } else if (idade < 21) {
                 //jovem
+                img.setAttribute('src', 'jovem-m.png')
             } else if (idade < 60) {
                 //adulto
+                img.setAttribute('src', 'adulto-m.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso-m.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
