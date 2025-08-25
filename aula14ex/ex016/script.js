ea535eb26f25a23eb73b005d9d6d5ee7288d376c
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-08-25 08:49:30 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,11 +2,19 @@ function contar() {
 var ini = document.getElementById('ini')
 var fim = document.getElementById('fim')
 var pas = document.getElementById('pas')
+var res = document.getElementById('res')
 
 if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
     window.alert('[ERRO] Faltam dados')
 } else {
-    alert('TUDO OK!')
+    res.innerHTML = 'Contando:  '
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(pas.value)
+
+    for(let c = i; c <= f; c+= p) {
+        res.innerHTML += `${c} `
+    }
 }
     
 
