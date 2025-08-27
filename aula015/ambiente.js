e446dc361674da59e2be1f6c903e736ad14a668a
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-08-27 08:58:19 -0300

    Update ambiente.js

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,4 +1,28 @@
 
-let num = [ ]
+let num = [5, 3]
 
-console.log(num)
+
+//num.push(1)
+
+//num.sort()
+
+//console.log(num)
+
+//console.log(`Nosso vetor tem ${num.length} posições.`)
+
+//console.log(`${num[1]}`)
+
+
+//num.length
+
+//num.sort()
+
+//num[9] = 0
+
+//num.push(7)
+
+
+
+
+
+
