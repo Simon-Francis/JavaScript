9a9a2b5cbf0f89d1ce29a831fb81a5b3f7ff28a2
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-08-27 15:39:28 -0300

    Create vetornatela.js

---
+++ aula015/vetornatela.js
@@ -0,0 +1,17 @@
+let valores = [8,1,7,4,2,9]
+
+console.log(valores)
+
+/*for(let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}.`)
+}
+
+
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}.`)
+} */
+
+//let pos = valores.indexOf(8)
+console.log(`O valor 8 está na posição ${valores.indexOf(2)}.`)
+
