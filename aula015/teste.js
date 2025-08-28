79749dc7fbe4225ec1130de45cb0bb3f43d7451c
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-08-28 08:16:54 -0300

    Update teste.js

--- aula015/teste.js
+++ aula015/teste.js
@@ -1,4 +1,10 @@
 
 let array = [2,8,4,6]
 
-console.log(`${array.indexOf(8)}`)
+//let pos = array.indexOf(5)
+
+if (array.indexOf(4) == -1) {
+    console.log(`Valor não encontrado.`)
+} else {
+    console.log(`${array.indexOf(4)}`)
+}
