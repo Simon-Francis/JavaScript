d67cfffe789c6f67fc1d9da5f9f13d1b0c7f8000
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-09-08 16:38:16 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -24,8 +24,8 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert(`Tudo Ok!`)
-} else {
+        
+    } else {
     window.alert(`Valor inválido ou já encontrado na lista.`)
 }
 }
