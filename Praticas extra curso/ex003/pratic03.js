46976eab58a2c2752c506226aee322d13f29d876
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-09-09 16:04:38 -0300

    ex003

---
+++ Praticas extra curso/ex003/pratic03.js
@@ -0,0 +1,15 @@
+let num = document.getElementById('num')
+let res = document.getElementById('res')
+
+
+function clicar() {
+    if (num.value.length == 0)
+        alert("Favor digitar um número primeiro")
+    else {
+        let valor = Number(num.value)
+        for (cont = 1, cont <= valor, cont ++) {
+            res.innerHTML = `${cont}`
+        }
+    }
+}
+num.focus()
